2e1be73fee3f49b011cac51ed5ec6c533d1f5226
GD <nannaya2121@hanmail.net>
2022-04-11 12:34:58 +0900

    더하기 모듈 완성했습니다.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import calc.Calculator;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -16,6 +18,10 @@ public class Main {
 		System.out.println("팀원 코드1");
 		System.out.println("팀원 코드2");
 		System.out.println("팀원 코드3");
+		
+		Calculator cal = new Calculator();
+		int addResult = cal.add(1, 2);
+		System.out.println(addResult);
 
 
 	}
