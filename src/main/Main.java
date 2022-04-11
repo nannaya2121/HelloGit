e20cc25675d9e38342518ab0e384d42f9493e81f
nannaya2121 <69185387+nannaya2121@users.noreply.github.com>
2022-04-11 12:21:15 +0900

    팀원이 12번라인부터 14번라인까지 코드완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("원격저장소 사용 가능2");
 		System.out.println("팀장 코드시작입니다.");
 		System.out.println("팀장 코드마지막입니다.");
+		
+		System.out.println("팀원 코드1");
+		System.out.println("팀원 코드2");
+		System.out.println("팀원 코드3");
 
 	}
 
