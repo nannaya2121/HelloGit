475ea88311892577b42c7940373c7239bfe2bd8f
GD <nannaya2121@hanmail.net>
2022-04-11 12:16:52 +0900

    팀장 커밋 완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("로컬저장소 사용");
 		System.out.println("원격저장소 사용 가능");
 		System.out.println("원격저장소 사용 가능2");
+		System.out.println("팀장 코드시작입니다.");
+		System.out.println("팀장 코드마지막입니다.");
 
 	}
 
